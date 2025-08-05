a46a9a42ab3335835a52b485b53d61497bd47621
zach <zmriel@gmail.com>
2025-08-05 13:13:33 -0400

    refactor: extract components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,12 @@
+<script>
+	import Search from './Search.svelte';
+</script>
+
 <div class="hero min-h-screen bg-base-200">
 	<div class="hero-content text-center">
-		<div class="flex max-w-lg flex-col gap-6">
-			<h1 class="text-5xl font-bold">Just Recipes</h1>
-			<label class="input">
-				<svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-					<g
-						stroke-linejoin="round"
-						stroke-linecap="round"
-						stroke-width="2.5"
-						fill="none"
-						stroke="currentColor"
-					>
-						<circle cx="11" cy="11" r="8"></circle>
-						<path d="m21 21-4.3-4.3"></path>
-					</g>
-				</svg>
-				<input type="search" class="grow" placeholder="Search" />
-			</label>
+		<div class="max-w-xl">
+			<h1 class="mb-6 text-5xl font-bold">Just Recipes</h1>
+			<Search />
 		</div>
 	</div>
 </div>
---
+++ src/routes/Search.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+	import CloseIcon from './CloseIcon.svelte';
+
+	let value = $state('');
+	let inputElement: HTMLInputElement;
+
+	/**
+	 * Clear all text
+	 */
+	export function clear() {
+		value = '';
+	}
+</script>
+
+<form action="/search" method="get" role="search" onreset={() => inputElement.focus()}>
+	<label
+		class={[
+			'input',
+			'shadow-md',
+			'transition-shadow',
+			'focus-within:shadow-lg',
+			'focus-within:outline-none'
+		]}
+	>
+		<svg
+			class="h-[1.5em] w-[1.5em] opacity-50"
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 24 24"
+		>
+			<g
+				stroke-linejoin="round"
+				stroke-linecap="round"
+				stroke-width="2.5"
+				fill="none"
+				stroke="currentColor"
+			>
+				<circle cx="11" cy="11" r="8"></circle>
+				<path d="m21 21-4.3-4.3"></path>
+			</g>
+		</svg>
+		<span class="sr-only">search term</span>
+		<!-- svelte-ignore a11y_autofocus -->
+		<input
+			autocomplete="off"
+			autocapitalize="none"
+			autocorrect="off"
+			type="text"
+			class="grow"
+			placeholder="blueberry muffins"
+			name="q"
+			minlength="1"
+			bind:value
+			bind:this={inputElement}
+		/>
+		{#if value.length > 0}
+			<button
+				class="h-full cursor-pointer p-1 transition-colors hover:text-base-content/70"
+				type="reset"
+			>
+				<CloseIcon class="h-[1em] w-[1em] opacity-50" />
+			</button>
+		{/if}
+	</label>
+	<button class="btn mt-6 w-40 btn-primary" type="submit">Search</button>
+</form>
